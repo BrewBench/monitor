eef33a04412b96d47647c51b8d3f7ff1bcb0f28c
Andrew Van Tassel <andrew@andrewvantassel.com>
2017-11-11 13:04:41 -0700

    Updated settings styles

--- build/assets/BrewBenchInfluxDBYun/BrewBenchInfluxDBYun.ino
+++ build/assets/BrewBenchInfluxDBYun/BrewBenchInfluxDBYun.ino
@@ -69,6 +69,10 @@ void process(BridgeClient client) {
     responseOkHeader(client);
     analogCommand(client);
   }
+  if (command == "settings") {
+    responseOkHeader(client);
+    settingsCommand(client);
+  }
   if (command == "DS18B20") {
     responseOkHeader(client);
     ds18B20Command(client);
@@ -133,6 +137,17 @@ void analogCommand(BridgeClient client) {
 
   // Send JSON response to client
   client.print("{\"pin\":\""+String(spin)+String(pin)+"\",\"value\":\""+String(value)+"\"}");
+}
+
+void settingsCommand(BridgeClient client) {
+  char spin = client.read();
+  String value = '';
+  
+  if (client.read() == '/') {
+
+  }
+  // Send JSON response to client
+  client.print("{\"setting\":\""+String(spin)+"\",\"value\":\""+String(value)+"\"}");
 }
 
 void ds18B20Command(BridgeClient client) {
